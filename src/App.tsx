7674eebebe0ea3e73f43aeb18ccaf85304685f40
AC4615 <AC4615@labranet.jamk.fi>
2024-10-29 20:21:04 +0200

    Fix TypeScript error by removing unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { fetchCryptoData } from './api/crypto';
 import { CryptoData } from './types';
 
